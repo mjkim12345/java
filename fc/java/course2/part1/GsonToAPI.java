fba1bddc6a87ed6786e6eeca36cc191eb7072748
user <offonoff6101@naver.com>
2024-09-27 18:05:37 +0900

    API

---
+++ fc/java/course2/part1/GsonToAPI.java
@@ -0,0 +1,10 @@
+package fc.java.fc.java.course2.part1;
+
+import fc.java.fc.java.model2.Person;
+
+public class GsonToAPI {
+    public static void main(String[] args) {
+        Person person = new Person("John",30);
+        
+    }
+}
